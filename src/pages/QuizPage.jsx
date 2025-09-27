6f5a2d93e2d75e9839407f514540c5cb2f4a2670
Aadhikesav <153075798+Aadhikesav07@users.noreply.github.com>
2025-09-27 18:17:41 +0530

    Create QuizPage.jsx

---
+++ src/pages/QuizPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import QuizComponent from "../components/quiz/QuizComponent";
+
+const QuizPage = () => (
+  <div>
+    <QuizComponent />
+  </div>
+);
+
+export default QuizPage;
