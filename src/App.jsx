cadd06fe3b4c45475160f2eceeb7a76d1a265d96
Aadhikesav <153075798+Aadhikesav07@users.noreply.github.com>
2025-09-28 11:07:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,38 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/common/Header.jsx";
-import Footer from "./components/common/Footer.jsx";
-import HomePage from "./pages/HomePage.jsx";
-import CourseCatalogPage from "./pages/CourseCatalogPage.jsx";
-import CourseDetails from "./components/course/CourseDetails.jsx";
-import LearningPage from "./pages/LearningPage.jsx";
-import QuizPage from "./pages/QuizPage.jsx";
-import StudentDashboardPage from "./pages/StudentDashboardPage.jsx";
-import InstructorDashboardPage from "./pages/InstructorDashboardPage.jsx";
-import CertificatePage from "./pages/CertificatePage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
-import RegisterPage from "./pages/RegisterPage.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from "./components/common/Header"
+import Footer from "./components/common/Footer"
+import HomePage from "./pages/HomePage"
+import CourseCatalogPage from "./pages/CourseCatalogPage"
+import LearningPage from "./pages/LearningPage"
+import QuizPage from "./pages/QuizPage"
+import StudentDashboardPage from "./pages/StudentDashboardPage"
+import InstructorDashboardPage from "./pages/InstructorDashboardPage"
+import CertificatePage from "./pages/CertificatePage"
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
 
-const App = () => (
-  <Router>
-    <Header />
-    <main className="min-h-screen">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/catalog" element={<CourseCatalogPage />} />
-        <Route path="/course/:courseId" element={<CourseDetails />} />
-        <Route path="/learning/:courseId" element={<LearningPage />} />
-        <Route path="/quiz/:courseId/:quizId" element={<QuizPage />} />
-        <Route path="/dashboard/student" element={<StudentDashboardPage />} />
-        <Route path="/dashboard/instructor" element={<InstructorDashboardPage />} />
-        <Route path="/certificate/:certificateId" element={<CertificatePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </main>
-    <Footer />
-  </Router>
-);
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1 p-4">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/catalog" element={<CourseCatalogPage />} />
+            <Route path="/learning/:id" element={<LearningPage />} />
+            <Route path="/quiz/:id" element={<QuizPage />} />
+            <Route path="/dashboard" element={<StudentDashboardPage />} />
+            <Route path="/instructor" element={<InstructorDashboardPage />} />
+            <Route path="/certificate/:id" element={<CertificatePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
 
-export default App;
+export default App
