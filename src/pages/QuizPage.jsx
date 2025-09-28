9303f8a01663fa7077daadec177206b16db412cf
Aadhikesav <153075798+Aadhikesav07@users.noreply.github.com>
2025-09-28 11:13:12 +0530

    Update QuizPage.jsx

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-import QuizComponent from "../components/quiz/QuizComponent";
+import { useParams } from "react-router-dom"
+import { useStore } from "../store/useStore"
+import QuizComponent from "../components/quiz/QuizComponent"
 
-const QuizPage = () => (
-  <div>
-    <QuizComponent />
-  </div>
-);
+export default function QuizPage() {
+  const { id } = useParams()
+  const { quizzes } = useStore()
+  const quiz = quizzes.find((q) => q.id === parseInt(id))
 
-export default QuizPage;
+  if (!quiz) return <p>Quiz not found</p>
+
+  return (
+    <div className="container mx-auto p-6">
+      <QuizComponent quiz={quiz} />
+    </div>
+  )
+}
