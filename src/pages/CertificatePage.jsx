6c0406285c0c12aacf360aee23fb0b8df91ea95d
Aadhikesav <153075798+Aadhikesav07@users.noreply.github.com>
2025-09-28 11:14:18 +0530

    Update CertificatePage.jsx

--- src/pages/CertificatePage.jsx
+++ src/pages/CertificatePage.jsx
@@ -1,14 +1,17 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import { useStore } from "../store/useStore";
-import CertificateComponent from "../components/user/CertificateComponent";
+import { useParams } from "react-router-dom"
+import { useStore } from "../store/useStore"
+import CertificateComponent from "../components/user/CertificateComponent"
 
-const CertificatePage = () => {
-  const { certificateId } = useParams();
-  const { certificates } = useStore();
-  const cert = certificates.find(c => c.id === Number(certificateId));
-  if (!cert) return <div className="mt-8 text-center">Certificate not found.</div>;
-  return <CertificateComponent cert={cert} />;
-};
+export default function CertificatePage() {
+  const { id } = useParams()
+  const { certificates } = useStore()
+  const cert = certificates.find((c) => c.courseId === parseInt(id))
 
-export default CertificatePage;
+  if (!cert) return <p>No certificate found.</p>
+
+  return (
+    <div className="container mx-auto p-6">
+      <CertificateComponent cert={cert} />
+    </div>
+  )
+}
